0f22be191c483abdb92da8dcf37a2b973aca91b4
sorachii <bencecube@gmail.com>
2021-06-13 00:18:01 +0200

    updated init.sh

--- init.sh
+++ init.sh
@@ -2,7 +2,8 @@
 
 # tmux, zsh, nvim
 if command -v apt > /dev/null; then
-	apt full-upgrade
+	apt update
+	apt upgrade
 	apt install zsh tmux sudo -y
 	# build requirements for neovim
 	apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
