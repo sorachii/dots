61976a8cbb69eb9933d88d68149689b57de71f26
sorachii <bencecube@gmail.com>
2021-06-26 23:26:02 +0200

    Updated init.sh

--- init.sh
+++ init.sh
@@ -1,37 +1,47 @@
 #!/usr/bin/env bash
 
-# tmux, zsh, nvim
+echo "Update system, then installing zsh, tmux and build tools for neovim."
+# Installing mytmux, zsh, nvim
 if command -v apt > /dev/null; then
 	apt update
 	apt upgrade -y
-	apt install zsh tmux sudo -y
+	sudo apt install zsh tmux -y
 	# build requirements for neovim
-	apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
+	sudo apt-get install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip -y
 elif command -v yum > /dev/null; then
-	yum upgrade
-	yum install zsh tmux
+	sudo yum upgrade
+	sudo yum install zsh tmux
 	# build requirements for neovim
-	yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext
+	sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip patch gettext
 elif command -v pacman > /dev/null; then
-	pacman -Syu
-	pacman -S zsh tmux
+	sudo pacman -Syu
+	sudo pacman -S zsh tmux
 	# build requirements for neovim
-	pacman -S base-devel cmake unzip ninja tree-sitter
+	sudo pacman -S base-devel cmake unzip ninja tree-sitter
 elif command -v apk > /dev/null; then
-	# build requirements for neovim
 	apk update
 	apk upgrade
-	apk add zsh
+	apk add zsh tmux
+	# build requirements for neovim
 	apk add build-base cmake automake autoconf libtool pkgconf coreutils curl unzip gettext-tiny-dev
 else
 	echo "Your distro is not between the supported ones."
 fi
 
+# If tpm can't be found, download it
+[ ! -d ${HOME}/.tmux/plugins/tpm  ] && git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
+
+# Building neovim from source
+if [ -d ~/git/neovim  ]; then
+	echo "Neovim is already installed"
+else
+	git clone https://github.com/neovim/neovim.git ~/git/neovim
+	cd ~/git/neovim
+	make
+	sudo make install
+fi
+
+# Copying zsh config then using it is necessary for
+# finishing the configuration and for bare git repo to work
 cp ~/git/dots/.zshrc ~
 chsh -s $(which zsh) $USER
-
-# building neovim from source
-git clone https://github.com/neovim/neovim.git ~/git/neovim
-cd ~/git/neovim
-make
-sudo make install
