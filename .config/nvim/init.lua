e47f09e6a320f8f3e2abd0d2d48111af345e0e40
sorachii <bencecube@gmail.com>
2021-09-20 19:36:49 +0200

    nvim: Tweaking gruvbox-flat(harder background).

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -26,8 +26,9 @@ require('packer').startup(function()
   use 'tpope/vim-commentary'
   use 'ludovicchabant/vim-gutentags'
   use {'nvim-telescope/telescope.nvim', requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}} }
-  use 'junegunn/seoul256.vim'
   use 'eddyekofo94/gruvbox-flat.nvim'
+  use 'junegunn/seoul256.vim'
+  use 'sainnhe/gruvbox-material'
   use 'ryanoasis/vim-devicons'
   use {'hoob3rt/lualine.nvim', requires = 'kyazdani42/nvim-web-devicons'}
   use {'nvim-treesitter/nvim-treesitter', branch = '0.5-compat', run = ':TSUpdate'}
@@ -72,11 +73,12 @@ vim.wo.signcolumn="yes"
 
 --Set colorscheme (order is important here)
 vim.o.termguicolors = true
+vim.g.background = dark
+vim.g.gruvbox_flat_style = "hard"
 vim.cmd[[colorscheme gruvbox-flat]]
 -- vim.g.seoul256_background = 236
--- vim.g.background = dark
 -- vim.cmd[[colorscheme seoul256]]
---vim.cmd[[colorscheme seoul256-light]]
+-- vim.cmd[[colorscheme seoul256-light]]
 
 --Thinkering with auto color change for lualine
 --vim.api.nvim_command("autocmd optionset background lua require('lualine').setup()")
