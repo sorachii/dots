9c348a4f1425223e2cf94bd0aef76fec1ee82da9
sorachii <bencecube@gmail.com>
2022-07-01 14:37:27 +0200

    Added new keymaps to jdtls.

--- .config/nvim/jdtls_setup.lua
+++ .config/nvim/jdtls_setup.lua
@@ -70,6 +70,7 @@ function M.setup()
       local opts = { noremap=true, silent=true }
       buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
       buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+      buf_set_keymap("n", "<leader>gt", "<Cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
       buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
       buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
       buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
@@ -85,7 +86,7 @@ function M.setup()
       buf_set_keymap('n', '<leader>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
       -- Java specific
       
-      buf_set_keymap("n", "<leader>dca", "<Cmd>lua require'jdtls'.code_action()<CR>", opts)
+      buf_set_keymap("n", "<leader>dca", "<Cmd>lua vim.lsp.buf.code_action()<CR>", opts)
       buf_set_keymap("n", "<leader>doi", "<Cmd>lua require'jdtls'.organize_imports()<CR>", opts)
       buf_set_keymap("n", "<leader>dtc", "<Cmd>lua require'jdtls'.test_class()<CR>", opts)
       buf_set_keymap("n", "<leader>dtm", "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", opts)
@@ -93,7 +94,7 @@ function M.setup()
       buf_set_keymap("n", "<leader>dev", "<Cmd>lua require'jdtls'.extract_variable()<CR>", opts)
       buf_set_keymap("v", "<leader>dem", "<Esc><Cmd>lua require'jdtls'.extract_method(true)<CR>", opts)
 
-      buf_set_keymap("n", "<leader>cf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+      buf_set_keymap("n", "<leader>dcf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 
       -- vim.api.nvim_exec([[
       --     hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
